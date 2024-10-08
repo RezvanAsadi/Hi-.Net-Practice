eed23ef36e2aeb2aa5c4fdde9c8c25bd35923349
REZVAN <REZVAN@REZVAN-PC>
2024-10-08 23:25:10 +0330

    Add project files.

---
+++ Hi .Net Practice/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Hi_.Net_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*int ageint = 10;
+short ageshort = 13;
+ageint = ageshort;
+ageshort = (short)ageint;
+
+string agestr = ageint.ToString();
+int ageint2 = int.Parse(agestr);
+
+
+Console.WriteLine("please enter your age:");
+int age= int.Parse(Console.ReadLine());
+if(age<15)
+{
+    Console.WriteLine("you are nat valid.");
+ 
+}
+else
+{
+    Console.WriteLine("you are valid,please stay more.");
+}
+return;*/
+
+
+        }
+    }
+}
